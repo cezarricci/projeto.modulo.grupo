d6abd91560a19a42511c61a6bca17c5c5adaa8aa
adviefe <adrianopvg@gmail.com>
2022-11-03 20:17:08 -0300

    Commit Classe Objeto Conta

---
+++ src/entities/ContaNova.java
@@ -0,0 +1,44 @@
+package entities;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class ContaNova {
+    private Integer IDConta = 0;
+    private String IDCliente = "00000000000000";
+    private String TipoConta = "CONTA_CORRENTE";
+    private BigDecimal Saldo = BigDecimal.valueOf(0).setScale(4, RoundingMode.HALF_EVEN);
+
+
+    public Integer getIDConta() {
+        return IDConta;
+    }
+
+    public void setIDConta(Integer IDConta) {
+        this.IDConta = IDConta;
+    }
+
+    public String getIDCliente() {
+        return IDCliente;
+    }
+
+    public void setIDCliente(String IDCliente) {
+        this.IDCliente = IDCliente;
+    }
+
+    public String getTipoConta() {
+        return TipoConta;
+    }
+
+    public void setTipoConta(String tipoConta) {
+        TipoConta = tipoConta;
+    }
+
+    public BigDecimal getSaldo() {
+        return Saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        Saldo = saldo;
+    }
+}
